5e0192eccf561f97bab282ec2bb3611b436062c5
Carlos Maita <carlosmaita2009@gmail.com>
2020-11-24 01:16:12 -0400

    ajustes funnel

--- resources/views/funnels/funnel01_ok.blade.php
+++ resources/views/funnels/funnel01_ok.blade.php
@@ -38,7 +38,7 @@
 @section('content')
 
     <!-- hero -->
-    <section class="">
+    <section class="pt-5">
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-lg-6 mt-5">
@@ -48,7 +48,7 @@
             </b></h1>
               <p class="text-center">La funcionalidad de <mark style="background-color: #8ed825;" >Instagram shopping</mark> te permite tener una tienda dentro de instagram.<br>
               </p>
-              <img class="d-sm-none" src="{{asset('imagen/funnels/tienda-instagram-01.jpg')}}" alt="tienda de instagram">
+              <img class="d-sm-none" src="{{asset('imagen/funnels/tienda-instagram-03.svg')}}" alt="tienda de instagram">
               <h2 class="text-center h5"><b>¡Te ayudaremos a configurar la tuya para Venezuela!</b></h2>
               <p class="text-center">
                <div class="row">
@@ -65,8 +65,8 @@
                   
               </p>
             </div>
-            <div class="col-lg-4 d-none d-sm-block">
-              <img src="{{asset('imagen/funnels/tienda-instagram-01.jpg')}}" alt="tienda de instagram">
+            <div class="col-lg-6 d-none d-sm-block text-center p-5">
+              <img src="{{asset('imagen/funnels/tienda-instagram-03.svg')}}" alt="tienda de instagram">
             </div>
           </div>
         </div>    
@@ -74,79 +74,19 @@
       <!-- / hero -->
   
       
-      
-      
-  
-      <!-- features -->
-      <section class="bg-primary separator-top separator-bottom" id="beneficios">
-        <div class="container text-white">
-          <div class="row">
-            <div class="col">
-              <h2><b>Beneficios</b> de tener una Tienda de instagram</h2>
-            </div>
-          </div>
-          <div class="row gutter-2 gutter-lg-4">
-            <div class="col-md-6 col-lg-4">
-              <div class="media align-items-center mb-2">
-                <i class="svg-icon fs-50 text-primary mr-3">
-                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-fill" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke="#8ED825" stroke-width="0.5" fill="none"  d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
-                  </svg>
-                </i>
-                <div class="media-body">
-                  <h5 class="fs-20 font-weight-normal"><b>PROMOCIÓN</b></h5>
-                </div>
-              </div>
-              <p>Promocionar tus productos directamente a los usuarios de Instagram a través de publicaciones e historias</p>
-            </div>
-            <div class="col-md-6 col-lg-4">
-              <div class="media align-items-center mb-2">
-                <i class="svg-icon fs-50 text-primary mr-3">
-                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke="#8ED825" stroke-width="0.5" fill="none"  d="M15 4H1v8h14V4zM1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1z"/>
-                    <path  stroke="#8ED825" stroke-width="0.5" fill="none"  d="M13 4a2 2 0 0 0 2 2V4h-2zM3 4a2 2 0 0 1-2 2V4h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 12a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
-                  </svg>
-                </i>
-                <div class="media-body">
-                  <h5 class="fs-20 font-weight-normal"><b>PRECIO</b></h5>
-                </div>
-              </div>
-              <p>Podrás mostrar precios de los productos presentados en las publicaciones y facilitar el proceso de compra a tus seguidores.</p>
-            </div>
-            <div class="col-md-6 col-lg-4">
-              <div class="media align-items-center mb-2">
-                <i class="svg-icon fs-60 text-primary mr-3">
-                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke="#8ED825" stroke-width="0.5" fill="none" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
-                  </svg>
-                </i>
-                <div class="media-body">
-                  <h5 class="fs-20 font-weight-normal"><b>CATALOGO</b></h5>
-                </div>
-              </div>
-              <p>Podrás integrar tu catálogo de productos con tu perfil de Instagram y mostrarlo en tu tienda virtual.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md-12 text-center">
-              <!-- Button trigger modal -->
-              <button type="button"class="btn btn-primary" data-toggle="modal" data-target="#modelId">
-                Ver Requisitos
-              </button>
-             
-            </div>
-          </div>
+      <div class="row">
+        <div class="col-12">
+          <p class="lead text-center"><b>¡Empecemos por lo primero!</b><br> Los <b style="color:#8ed825">REQUISITOS</b> para tu tienda de Instagram.<br></p>
         </div>
-      </section>
-      <!-- / features -->
-
+      </div>
+    
       
     <!-- blog -->
     <section class="separator-bottom" id="requisitos">
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-8">
-            <h2><b>Requisitos</b> para Tienda Instagram</h2>
+            <h2 class="text-center"><b>Requisitos</b> para Tienda Instagram</h2>
           </div>
         </div>
         <div class="row justify-content-center">
@@ -197,7 +137,8 @@
         </div>
         <div class="row">
           <div class="col-12">
-            <p class="lead text-center"><b>¿Sabias que necesitas un sitio web?</b><br>mmm... Tenemos <b style="color:#8ed825">ASESORÍA + SITIO WEB</b> para tu tienda de Instagram.<br></p>
+            <p class="lead text-center"><b>¿Sabias que necesitas un sitio web?</b><br>mmm... Tenemos <b style="color:#8ed825">ASESORÍA + SITIO WEB</b> para tu tienda de Instagram.<br>
+              <a href="https://api.whatsapp.com/send?phone=584143446225&text=Hola,%20Quiero%20mi%20tienda%20de%20Instagram%20en%20promoción%20con%20la%20ecommerce.%20" class="btn btn-rounded btn-primary ml-1">Promoción</a></p>
           </div>
         </div>
       </div>
@@ -268,8 +209,72 @@
           </div>
         </div>
       </section>
-
-     
+{{-- 
+      <div class="row">
+        <div class="col-12">
+          <p class="lead text-center"><b>¿Qué beneficios obtendré?</b><br> Te lo contamos  <b style="color:#8ed825">TODO...</b><br></p>
+        </div>
+      </div> --}}
+  
+      <!-- features -->
+      <section class="bg-primary separator-top separator-bottom" id="beneficios">
+        <div class="container text-white">
+          <div class="row">
+            <div class="col">
+              <h2>Algunos <b>BENEFICIOS...</b></h2>
+            </div>
+          </div>
+          <div class="row gutter-2 gutter-lg-4">
+            <div class="col-md-6 col-lg-4">
+              <div class="media align-items-center mb-2">
+                <i class="svg-icon fs-50 text-white mr-3">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-bag-fill" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke="#8ED825" stroke-width="0.5" fill="none"  d="M8 1a2.5 2.5 0 0 0-2.5 2.5V4h5v-.5A2.5 2.5 0 0 0 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5z"/>
+                  </svg>
+                </i>
+                <div class="media-body">
+                  <h5 class="fs-20 font-weight-normal"><b>PROMOCIÓN</b></h5>
+                </div>
+              </div>
+              <p class="text-white">Podrás promocionar tus productos directamente a los usuarios de Instagram a través de publicaciones e historias</p>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="media align-items-center mb-2">
+                <i class="svg-icon fs-50 text-white mr-3">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-cash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke="#8ED825" stroke-width="0.5" fill="none"  d="M15 4H1v8h14V4zM1 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H1z"/>
+                    <path  stroke="#8ED825" stroke-width="0.5" fill="none"  d="M13 4a2 2 0 0 0 2 2V4h-2zM3 4a2 2 0 0 1-2 2V4h2zm10 8a2 2 0 0 1 2-2v2h-2zM3 12a2 2 0 0 0-2-2v2h2zm7-4a2 2 0 1 1-4 0 2 2 0 0 1 4 0z"/>
+                  </svg>
+                </i>
+                <div class="media-body">
+                  <h5 class="fs-20 font-weight-normal"><b>PRECIO</b></h5>
+                </div>
+              </div>
+              <p class="text-white">Podrás mostrar precios de los productos presentados en las publicaciones y facilitar el proceso de compra a tus seguidores.</p>
+            </div>
+            <div class="col-md-6 col-lg-4">
+              <div class="media align-items-center mb-2">
+                <i class="svg-icon fs-60 text-white mr-3">
+                  <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-book" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke="#8ED825" stroke-width="0.5" fill="none" d="M1 2.828v9.923c.918-.35 2.107-.692 3.287-.81 1.094-.111 2.278-.039 3.213.492V2.687c-.654-.689-1.782-.886-3.112-.752-1.234.124-2.503.523-3.388.893zm7.5-.141v9.746c.935-.53 2.12-.603 3.213-.493 1.18.12 2.37.461 3.287.811V2.828c-.885-.37-2.154-.769-3.388-.893-1.33-.134-2.458.063-3.112.752zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783z"/>
+                  </svg>
+                </i>
+                <div class="media-body">
+                  <h5 class="fs-20 font-weight-normal"><b>CATALOGO</b></h5>
+                </div>
+              </div>
+              <p class="text-white">Podrás integrar tu catálogo de productos con tu perfil de Instagram y mostrarlo en tu tienda virtual.</p>
+            </div>
+          </div>
+         
+          <div class="row">
+            <div class="col-12">
+              <p class="lead text-center"><b>¿Estás Listo?</b> Si, ¡Quiero mi tienda!. <a href="https://api.whatsapp.com/send?phone=584143446225&text=Hola,%20Quiero%20mi%20tienda%20de%20Instagram.%20" class="btn btn-rounded btn-white ml-1">Ir a una conversación</a></p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- / features -->
 
       
 @endsection
