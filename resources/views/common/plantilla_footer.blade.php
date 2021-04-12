ceaedc57092a1b43f5cc110a858e7067512f82a5
Carlos Maita <Carlosmaita2009@gmail.com>
2021-04-12 15:27:49 -0400

    agregar tawk

--- resources/views/common/plantilla_footer.blade.php
+++ resources/views/common/plantilla_footer.blade.php
@@ -73,3 +73,17 @@
 <script type="text/javascript">
   AOS.init();
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+  (function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/60749e9e067c2605c0c1bb3d/1f33on73t';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+  })();
+  </script>
+  <!--End of Tawk.to Script-->
